e8e142cd55b43dc6b9c78f11d19647b096fb3d0f
theanh <tranluutheanh17601@gmail.com>
2022-11-23 09:52:46 +0700

    update

--- server/src/test/lauches.test.js
+++ server/src/test/lauches.test.js
@@ -1,58 +1,86 @@
-const app = require('../app');
-const request = require('supertest');
+const app = require("../app");
+const request = require("supertest");
+const { mongoConnect, mongoDisconnect } = require("../services/mongo");
+const { loadPlanetsData } = require("../models/planets.model");
 
-describe('GET /lauches', function () {
-    it('responds with json', function () {
-        return request(app)
-            .get('/launches/')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                expect(response.body).toBeInstanceOf(Array);
-            })
-    });
+describe("Launches API", () => {
+	beforeAll(async () => {
+		await mongoConnect();
+		await loadPlanetsData();
+	});
+
+	afterAll(async () => {
+		await mongoDisconnect();
+	});
+
+	describe("Test GET /launches", () => {
+		test("It should respond with 200 success", async () => {
+			const response = await request(app)
+				.get("/launches")
+				.expect("Content-Type", /json/)
+				.expect(200);
+		});
+	});
+
+	describe("Test POST /launch", () => {
+		const completeLaunchData = {
+			mission: "USS Enterprise",
+			rocket: "NCC 1701-D",
+			target: "Kepler-62 f",
+			launchDate: "January 4, 2028",
+		};
+
+		const launchDataWithoutDate = {
+			mission: "USS Enterprise",
+			rocket: "NCC 1701-D",
+			target: "Kepler-62 f",
+		};
+
+		const launchDataWithInvalidDate = {
+			mission: "USS Enterprise",
+			rocket: "NCC 1701-D",
+			target: "Kepler-62 f",
+			launchDate: "zoot",
+		};
+
+		test("It should respond with 201 created", async () => {
+			const response = await request(app)
+				.post("/launches")
+				.send(completeLaunchData)
+				.expect("Content-Type", /json/)
+				.expect(201);
+
+			const requestDate = new Date(
+				completeLaunchData.launchDate
+			).valueOf();
+			const responseDate = new Date(response.body.launchDate).valueOf();
+			expect(responseDate).toBe(requestDate);
+
+			expect(response.body).toMatchObject(launchDataWithoutDate);
+		});
+
+		test("It should catch missing required properties", async () => {
+			const response = await request(app)
+				.post("/launches")
+				.send(launchDataWithoutDate)
+				.expect("Content-Type", /json/)
+				.expect(400);
+
+			expect(response.body).toStrictEqual({
+				error: "Missing required launch property",
+			});
+		});
+
+		test("It should catch invalid dates", async () => {
+			const response = await request(app)
+				.post("/launches")
+				.send(launchDataWithInvalidDate)
+				.expect("Content-Type", /json/)
+				.expect(400);
+
+			expect(response.body).toStrictEqual({
+				error: "Invalid launch date",
+			});
+		});
+	});
 });
-
-describe('POST /launches', function () {
-    it('responds with success', function () {
-        return request(app)
-            .post('/launches/')
-            .send({
-                mission: 'USS Enterprise',
-                rocket: 'NCC-1701-D',
-                target: 'Kepler-186 f',
-                launchDate: 'October 31, 2022',
-            })
-            .expect(201)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    mission: 'USS Enterprise',
-                    rocket: 'NCC-1701-D',
-                    target: 'Kepler-186 f',
-                    launchDate: '2022-10-30T17:00:00.000Z',
-                    upcoming: true,
-                    success: true,
-                });
-            });
-    });
-});
-
-describe('DELETE /launches', function () {
-    it('responds with success', function () {
-        return request(app)
-            .delete('/launches/101')
-            .expect(200)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    mission: 'USS Enterprise',
-                    rocket: 'NCC-1701-D',
-                    target: 'Kepler-186 f',
-                    launchDate: '2022-10-30T17:00:00.000Z',
-                    upcoming: false,
-                    success: false,
-                });
-            });
-    });
-});
-
